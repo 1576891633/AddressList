d1e95c24d84461d736896bf15553183fab877e60
1576891633 <34237334+1576891633@users.noreply.github.com>
2017-12-15 16:54:50 +0800

    Update AddressListBoot.java

--- src/main/java/com/lensyn/addresslist/AddressListBoot.java
+++ src/main/java/com/lensyn/addresslist/AddressListBoot.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * SpringBoot启动程序
+ * SpringBoot启动程序哦
  * Created by lizhongfu on 14:59 2017/12/15
  */
 @SpringBootApplication
