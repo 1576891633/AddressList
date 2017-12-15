16d63ebc59790ac6f49ef465df5806d5cad52957
1576891633 <1576891633@qq.com>
2017-12-15 16:53:19 +0800

    Initial commit

---
+++ src/main/java/com/lensyn/addresslist/AddressListBoot.java
@@ -0,0 +1,17 @@
+package com.lensyn.addresslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot启动程序
+ * Created by lizhongfu on 14:59 2017/12/15
+ */
+@SpringBootApplication
+public class AddressListBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressListBoot.class, args);
+    }
+}
+
