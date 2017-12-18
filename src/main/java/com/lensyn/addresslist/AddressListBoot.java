6b7aa5d7c0e6217c8efd6843b14c986bbe8052b3
1576891633 <1576891633@qq.com>
2017-12-18 09:31:58 +0800

    first commit

--- src/main/java/com/lensyn/addresslist/AddressListBoot.java
+++ src/main/java/com/lensyn/addresslist/AddressListBoot.java
@@ -2,12 +2,14 @@ package com.lensyn.addresslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * SpringBoot启动程序
  * Created by lizhongfu on 14:59 2017/12/15
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class AddressListBoot {
 
     public static void main(String[] args) {
